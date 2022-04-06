eb8f1578de7edb6ca7e411d3e62946146a1bfd7b
M. Sigid Prasetyo <sigid.04@students.amikom.ac.id>
2022-04-06 23:17:50 +0700

    ini project ReactNative pertama saya

---
+++ src/coba.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class coba extends Component {
+  static propTypes = {second: third}
+ color : ''
+  render() {
+    return (
+      <div>coba
+
+      </div>
+    )
+  }
+}
